574bb134beb83b2f19800fea12121ecb7dd94e5d
Alexandr Burdelev <alexbur228@gmail.com>
2021-10-14 16:50:56 +0300

    Добален navigation bar

--- app/src/main/java/com/example/myapplication/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/presentation/main/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.myapplication.presentation.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.presentation.findweather.FindWeatherFragment
+import com.example.myapplication.presentation.storageweather.StorageWeatherFragment
 
-class MainActivity : AppCompatActivity()  {
+class MainActivity : AppCompatActivity() {
 
     private var isFirstActivityCreated = true
+    private val viewBinding by viewBinding(ActivityMainBinding::bind)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +24,31 @@ class MainActivity : AppCompatActivity()  {
                 .replace(R.id.fragment_container, FindWeatherFragment.newInstance())
                 .commit()
             isFirstActivityCreated = false
+        }
+
+        with(viewBinding) {
+            bottomNavigation.setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.find -> {
+                        if (supportFragmentManager.fragments.find { it is FindWeatherFragment } == null) {
+                            supportFragmentManager.beginTransaction()
+                                .replace(R.id.fragment_container, FindWeatherFragment.newInstance())
+                                .commit()
+                        }
+                    }
+                    R.id.history -> {
+                        if (supportFragmentManager.fragments.find { it is StorageWeatherFragment } == null) {
+                            supportFragmentManager.beginTransaction()
+                                .replace(
+                                    R.id.fragment_container,
+                                    StorageWeatherFragment.newInstance()
+                                )
+                                .commit()
+                        }
+                    }
+                }
+                return@setOnItemSelectedListener true
+            }
         }
     }
 
